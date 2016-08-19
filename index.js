b7e2626201dbac8a0ed3def44dee672092dcefeb
sarahneely <sporter1990@hotmail.com>
2016-08-18 18:17:28 -0700

    big commit for all of the code that works

--- index.js
+++ index.js
@@ -4,67 +4,40 @@ var app = new alexa.app();
 app.launch(function(request,response) {
 	response.say('woof woof!');
 	response.card("woof woof!","This is an example card");
+	response.shouldEndSession(false);
 });
 
 app.intent("dogDoor", {
 	// slots
-	'slots':{"open":"OPEN"},
-    'utterances':[ '{open} the door' ],
+	'slots':{"status":"STATUS"},
+    'utterances':[ '{open} the door', '{close} the door' ],
 
-    'slots':{"close":"CLOSE"},
-    'utterances':[ '{close} the door' ]
 },
-
  function(request,response) {
-	// some code
-	var open = request.slot("open")
-	var close = request.slot("close")
-	switch(position) {
-		case"open":
+	 	var status;
+		switch(request.slot('status')) {
+
+		case'open':
 		// url for ngrog
-		response.say("opening the dog door");
-		response.send();
+		status='opening';
 		break;
 
-		case"close":
+		case'close':
 		// url for ngrog
-		response.say("closing the dog door");
-		response.send();
+		status='closing';
 		break;
 
 		default:
 		// url for ngrog
-		response.say("error occured");
-		response.send();
+		status='You broke me';
 		break;
 
-	response.shouldEndSession(true);
-    response.send();
-	}
+	};
+	 response.say(status);
+	 response.shouldEndSession(true);
+   response.send();
 });
 
-
-	app.intent('checkStatus', 
-	{
-    	'utterances':[ 
-    		'status check', 'what is the status', 'tell me the status'
-    	]
-  	},
-	function(request,response) {
-		setTimeout(function() {		// simulate an async request
-
-	        // This is async and will run after a brief delay
-	        response.say('Status is operational, mam!');
-	    
-	        // Must call send to end the original request
-	        response.send();
-		
-		}, 250);
-
-	    // Return false immediately so alexa-app doesn't send the response
-	    return false;
-	}
-);
 
 	/**
  * Error handler for any thrown errors.
